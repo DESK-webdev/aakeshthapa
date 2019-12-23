9a1a305227aee2ff4d8afa374867cc0b7dffebff
aakeshthapa <ageshbahadurthapa002@gmail.com>
2019-12-23 12:04:35 +0545

    added a new file

---
+++ htmlfile/php/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>website developing</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<link rel="stylesheet" type="text/css" href="style/2.css">
+<body>
+<nav class="navbar container-fluid navbar-expand-sm navbar-dark bg-dark">
+        <div class="container text-center ml-auto">
+            <a href="image.jpg" class="navbar-brand"><img src="image.jpg" alt="private" style="width: 50px;height: 50px;"></a>
+            <a href="#" class="navbar-brand">kaziaakusamu</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarid">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarid">
+        <ul class="navbar-nav ">
+            <li class="nav-list">
+                <a href="index.html"" class="nav-link"> Home</a>
+            </li>
+            <li class="nav-list">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-list">
+                <a href="#" class="nav-link">Gallery</a>
+            </li>
+            <li class="nav-list">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+        </ul>
+       
+       </div>
+    </div>
+    </nav>
+    <nav class="container-fluid bg-secondary">
+<h1> welcome to our website</h1>
+<p><strong>its me <abbr title="Agesh Bahadur Thapa">A.B Thapa </strong></abbr> </p>
+
+<p> click the photo then it download</p>
+<a href="sat.jpg" download="aaku" >
+<img src="sat.jpg" alt="satnogs" /></a>
+<ul>
+	<li><a href="#cd"> cd </a></li>
+	<li>dvd</li>
+	<li>harddisk</li>
+</ul>
+<h2>
+<a href ="login.php" target="_blank" title="login here">login here</a>
+</h2>
+
+<br>
+<br>
+<br>
+<br>
+
+<a name="cd">cd is here now</a>
+</nav>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ htmlfile/php/java/script/1.js
@@ -0,0 +1,11 @@
+const output = fizzBuzz(5);
+console.log(output);
+
+function fizzBuzz(input){
+}
+const person={
+    name:'aakesh',
+    age:19
+};
+for(let key in person)
+console.log(key,person[key]);
